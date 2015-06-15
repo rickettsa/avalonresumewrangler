a864574167b3d4fcd3cb431d348c16985ca6c0b6
Abraham Michael Becker <beckera@avalonconsult.com>
2015-06-15 15:31:47 -0400

    Got login button working and retrieved profile meta from Google.

--- front-end/src/common/directives/user-login.js
+++ front-end/src/common/directives/user-login.js
@@ -10,30 +10,37 @@ angular.module('resumeWrangler')
     return {
       templateUrl: 'templates/user-login.tpl.html',
       restrict: 'E',
-      scope: {},
+      scope: true,
       controller: function($scope, $state, $rootScope) {
         $scope.loggedIn = 0;
 
-        $scope.onSignIn = function(googleUser) {
+        //not scoped on purpose so it works with Google login button
+        function onSignIn(googleUser) {
           var profile = googleUser.getBasicProfile();
           $rootScope.userProfile = {};
           $rootScope.userProfile.id = profile.getId();
           $rootScope.userProfile.name = profile.getName();
           $rootScope.userProfile.biopic = profile.getImageUrl();
           $rootScope.userProfile.email = profile.getEmail();
+          $scope.loggedIn = 1;
           console.log('ID: ' + profile.getId());
           console.log('Name: ' + profile.getName());
           console.log('Image URL: ' + profile.getImageUrl());
           console.log('Email: ' + profile.getEmail());
           $rootScope.$broadcast('userLogin', {"userProfile": $rootScope.userProfile });
+          $scope.$apply();
         };
+        window.onSignIn = onSignIn;
+
 
         $scope.signOut = function() {
           var auth2 = gapi.auth2.getAuthInstance();
           auth2.signOut().then(function() {
-            console.log('User signed out.');
+            $scope.loggedIn = 0;
+            $scope.$apply();
           });
         };
+
       }
     };
   });
