6af0144e03f137b2b69c8331ed3ab7401838fe2c
Karl Erisman <erismank@avalonconsult.com>
2015-09-09 17:54:52 -0500

    Add functionality to work with resumes, projects, and users. Add directory for sample data.

--- back-end/api/app/app.py
+++ back-end/api/app/app.py
@@ -4,16 +4,18 @@ import json
 
 from datalayer import DataLayer
 
-ES_INDEX = 'resumes'
-RESUME_TYPE = 'resume'
-
 app = Flask(__name__)
 
 dl = DataLayer.factory('Elasticsearch')
 
 #-------
 
-def _create_resume(id=None):
+def _create_entity(creation_function, id=None):
+    '''Look at the request object to find JSON in the body.
+    From this, create a document in Elasticsearch. The provided creation
+    function is used to create the document. This will determine the type
+    of document created (a resume, project, etc.).'''
+
     if request.headers['Content-Type'] == 'application/json':
         if not request.json:
             abort(400)
@@ -23,73 +25,133 @@ def _create_resume(id=None):
         if not request.json:
             abort(400)
 
-        resume = jdata
+        entity = jdata
 
-        if(id is not None):
-            dl.create_resume(resume, id)
+        if(id is None):
+            id = creation_function(entity)
         else:
-            dl.create_resume(resume)
-        
-        return ''
+            creation_function(entity, id)
 
-# Create a resume
-#   POST http://localhost:5000/api/resumes
-#
+        return id
+
+#------- Users
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    id = _create_entity(dl.create_or_update_user)
+    return make_response('ok', 200, {'id': id} )
+
+@app.route('/api/users/<id>', methods=['PUT'])
+def update_user(id):
+    _create_entity(dl.create_or_update_user, id)
+    return ''
+
+@app.route('/api/users/search', methods=['GET'])
+def find_users():
+    un = request.args.get('username')
+    fn = request.args.get('firstname')
+    ln = request.args.get('lastname')
+    results = dl.find_users(username=un, firstname=fn, lastname=ln)
+    return jsonify( results )
+
+@app.route('/api/users', methods=['GET'])
+def list_users():
+    users = dl.list_users()
+    return make_response( json.dumps(users), 200, {'Content-Type': 'application/json'} )
+
+@app.route('/api/users/<id>', methods=['GET'])
+def get_user(id):
+    if len(id) == 0:
+        abort(404)
+    else:
+        user = dl.get_user(id)
+        return jsonify( user )
+
+@app.route('/api/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    dl.delete_user(id)
+    return ''
+
+#------- Resumes
+
 @app.route('/api/resumes', methods=['POST'])
 def create_resume():
-    return _create_resume()
+    id = _create_entity(dl.create_or_update_resume)
+    return ''
 
-# Update a resume
-#   PUT http://localhost:5000/api/resumes/AU2W0hYTw_8sJsjY6vi8
-#
 @app.route('/api/resumes/<id>', methods=['PUT'])
 def update_resume(id):
-    return _create_resume(id)
+    _create_entity(dl.create_or_update_resume, id)
+    return ''
 
-# List resumes
-#   GET http://localhost:5000/api/resumes/
-#
 @app.route('/api/resumes', methods=['GET'])
 def list_resumes():
     resumes = dl.list_resumes()
-    return jsonify( resumes )
+    return make_response( json.dumps(resumes), 200, {'Content-Type': 'application/json'} )
 
-#
-# Search for resumes
-#   GET http://localhost:5000/api/resumes/search?lastname=mefford&skill=java&skill=python
-#
-#FIXME: add search by project
-#
-#FIXME: improvements
-#   - full text search with scoring
-#   - pagination
-#   - sorting, faceting, etc.
-#   - snippet highlighting?
-#
 @app.route('/api/resumes/search', methods=['GET'])
 def find_resumes():
+    #FIXME: add search by project
+    #
+    #FIXME: improvements
+    #   - full text search with scoring
+    #   - pagination
+    #   - sorting, faceting, etc.
+    #   - snippet highlighting?
     fn = request.args.get('firstname')
     ln = request.args.get('lastname')
     skills = request.args.getlist('skill')
     results = dl.find_resumes(firstname=fn, lastname=ln, skills=skills)
     return jsonify( results )
 
-# Get a specific resume
-#   GET http://localhost:5000/api/resumes/AU2W0hYTw_8sJsjY6vi8
-#
 @app.route('/api/resumes/<id>', methods=['GET'])
 def get_resume(id):
-    resume = dl.get_resume(id)
     if len(id) == 0:
         abort(404)
-    return jsonify( resume )
+    else:
+        resume = dl.get_resume(id)
+        return jsonify( resume )
 
-# Delete a specific resume
-#   DELETE http://localhost:5000/api/resume/AU2W0hYTw_8sJsjY6vi8
-#
 @app.route('/api/resumes/<id>', methods=['DELETE'])
 def delete_resume(id):
     dl.delete_resume(id)
+    return ''
+
+#------- Projects
+
+@app.route('/api/projects', methods=['POST'])
+def create_project():
+    id = _create_entity(dl.create_or_update_project)
+    return ''
+
+@app.route('/api/projects/<id>', methods=['PUT'])
+def update_project(id):
+    _create_entity(dl.create_or_update_project, id)
+    return ''
+
+@app.route('/api/projects/search', methods=['GET'])
+def find_projects():
+    client_name = request.args.get('client_name')
+    start_date_min = request.args.get('start_date_min')
+    start_date_max = request.args.get('start_date_max')
+    end_date_min = request.args.get('end_date_min')
+    end_date_max = request.args.get('end_date_max')
+    summary = request.args.get('summary')
+    project_skills = request.args.getlist('project_skills')
+    results = dl.find_projects( client_name, start_date_min, start_date_max, end_date_min, end_date_max, summary, project_skills )
+    return jsonify( results )
+
+@app.route('/api/projects/<id>', methods=['GET'])
+def get_project(id):
+    if len(id) == 0:
+        abort(404)
+    else:
+        project = dl.get_project(id)
+        return jsonify(project)
+
+@app.route('/api/projects/<id>', methods=['DELETE'])
+def delete_project(id):
+    dl.delete_project(id)
     return ''
 
 #-------
--- back-end/api/app/datalayer/es.py
+++ back-end/api/app/datalayer/es.py
@@ -4,7 +4,7 @@ from elasticsearch import Elasticsearch, exceptions
 
 #-------
 
-# Combine the given "must" queries into a bool query
+# Combine several "must" queries into a bool query
 def bool_query( must_queries ):
     return {
         'query': {
@@ -14,6 +14,7 @@ def bool_query( must_queries ):
 
 def filtered_term_query(term, value): return { 'filtered': { 'filter': { 'term': { term: value } } } }
 
+#FIXME: change 'Competency' to 'Skill' or 'Skills'
 def nested_query(s):
     return { 'nested': {
             'path': 'Competency',
@@ -31,6 +32,12 @@ class ESDataLayer(DataLayer):
     RESUME_INDEX = 'resumes'
     RESUME_TYPE = 'resume'
 
+    PROJECT_INDEX = 'projects'
+    PROJECT_TYPE = 'project'
+
+    USER_INDEX = 'users'
+    USER_TYPE = 'user'
+
     def __init__(self):
         self.es = Elasticsearch()
 
@@ -40,26 +47,63 @@ class ESDataLayer(DataLayer):
     def create_mapping(self, mapping):
         self.es.indices.put_mapping(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, body=mapping)
 
-    def create_resume(self, resume, id=None):
+    #------- Users
+
+    def create_or_update_user(self, user, id=None):
+        doc_id = None
+        if id is None:
+            doc_id = DataLayer.uuid()
+        else:
+            doc_id = id
+
+        self.es.index(index=self.USER_INDEX, doc_type=self.USER_TYPE, body=user, id=doc_id)
+        return doc_id
+
+    def list_users(self):
+        #FIXME:support pagination or figure out a better way to get all results
+        users = self.es.search(index=self.USER_INDEX, doc_type=self.USER_TYPE, size=999999999)
+        return [ u for u in users['hits']['hits'] ]
+
+    def find_users(self, username, firstname, lastname):
+        un_query = fn_query = ln_query = None
+        if username is not None: un_query = filtered_term_query( 'username', username.lower() )
+        if firstname is not None: fn_query = filtered_term_query( 'firstName', firstname.lower() )
+        if lastname is not None: ln_query = filtered_term_query( 'lastName', lastname.lower() )
+        queries = filter( lambda q: q is not None, [un_query, fn_query, ln_query] )
+        combined_query = bool_query( queries )
+        return self.es.search(index=self.USER_INDEX, doc_type=self.USER_TYPE, body=combined_query)['hits']
+
+    def get_user(self, id):
+        user = self.es.get(index=self.USER_INDEX, doc_type=self.USER_TYPE, id=id)
+        return user
+
+    def delete_user(self, id):
+        self.es.delete(index=self.USER_INDEX, doc_type=self.USER_TYPE, id=id)
+
+    #------- Resumes
+
+    def create_or_update_resume(self, resume, id=None):
         # Generate UUID for new resume: default to DataLayer's UUID but allow caller to
         # pass in a custom UUID. Doing it this way instead of using named args because
         # DataLayer.uuid() needs to be called again for every call to create_resume()
-        uuid = None
+        doc_id = None
         if id is None:
-            uuid = DataLayer.uuid()
+            doc_id = DataLayer.uuid()
         else:
-            uuid = id
+            doc_id = id
 
-        self.es.index(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, body=resume, id=uuid)
-
-    def update_resume(self, resume, id):
-        self.create_resume(resume, id)
+        self.es.index(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, body=resume, id=doc_id)
+        return doc_id
 
     def list_resumes(self):
-        resumes = self.es.search(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE)['hits']
-        return resumes
+        #FIXME:
+        # - hard coding a magic number for now; need to either support paging or find out the proper way to get all results
+        # - is "list resume" functionality even needed?
+        resumes = self.es.search(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, size=999999999, _source=False)
+        return [ r for r in resumes['hits']['hits'] ]
 
-#NOTE: using variable.lower() in much of this code because ES mapping stores lowercased text in many cases as part of its normalization
+    #NOTE: using variable.lower() in much of this code because ES mapping stores lowercased text in many cases as part of its normalization
+    # (there may be better ways to index the data or other query types to use that don't require this step)
 
     def find_resumes(self, firstname, lastname, skills=[]):
         fn_query = ln_query = None
@@ -70,26 +114,91 @@ class ESDataLayer(DataLayer):
         combined_query = bool_query( queries )
         return self.es.search(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, body=combined_query)['hits']
 
-#######
-#FIXME:find_by_* not needed if find_resumes() can handle everything
-    def find_by_name(self, firstname, lastname):
-        fn_query = ln_query = None
-        if firstname is not None: fn_query = filtered_term_query( 'firstName', firstname.lower() )
-        if lastname is not None: ln_query = filtered_term_query( 'lastName', lastname.lower() )
-        name_query = bool_query( filter( lambda q: q is not None, [fn_query, ln_query] ) )
-        return self.es.search(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, body=name_query)['hits']
-
-    def find_by_skill(self, skills):
-        skill_query = bool_query( [nested_query(s) for s in skills ] )
-        return self.es.search(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, body=skill_query)['hits']
-#######
-
     def get_resume(self, id):
         resume = self.es.get(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, id=id)
         return resume
 
     def delete_resume(self, id):
         self.es.delete(index=self.RESUME_INDEX, doc_type=self.RESUME_TYPE, id=id)
+
+    #------- Projects
+
+    def create_or_update_project(self, project, id=None):
+        doc_id = None
+        if id is None:
+            doc_id = DataLayer.uuid()
+        else:
+            doc_id = id
+
+        self.es.index(index=self.PROJECT_INDEX, doc_type=self.PROJECT_TYPE, body=project, id=doc_id)
+        return doc_id
+
+    def find_projects(self, client_name, start_date_min, start_date_max, end_date_min, end_date_max, summary, project_skills):
+        skill_queries = []
+        for s in project_skills:
+            skill_queries += [ { "match": { "projectSkills": s } } ]
+        if len(skill_queries) == 0:
+            skill_queries = None
+
+        client_name_query = start_date_query = end_date_query = summary_query = None
+        if client_name is not None:
+            client_name_query = { "match": {
+               "clientName": client_name
+            }}
+        if start_date_min is not None and start_date_max is not None:
+            start_date_query = { "range": {
+               "startDate": {
+                  "from": start_date_min,
+                  "to": start_date_max
+               }
+            }}
+        elif start_date_min is not None:
+            start_date_query = { "range": {
+               "startDate": {
+                  "from": start_date_min,
+               }
+            }}
+        elif start_date_max is not None:
+            start_date_query = { "range": {
+               "startDate": {
+                  "to": start_date_max
+               }
+            }}
+        if end_date_min is not None and end_date_max is not None:
+            end_date_query = { "range": {
+               "endDate": {
+                  "from": end_date_min,
+                  "to": end_date_max
+               }
+            }}
+        elif end_date_min is not None:
+            end_date_query = { "range": {
+               "endDate": {
+                  "from": end_date_min,
+               }
+            }}
+        elif end_date_max is not None:
+            end_date_query = { "range": {
+               "endDate": {
+                  "to": end_date_max
+               }
+            }}
+        if summary is not None:
+            summary_query = { "match_phrase": {
+               "summary": summary
+            }}
+        must_queries = filter( lambda x: x is not None, [ client_name_query, start_date_query, end_date_query, summary_query, skill_queries ] )
+        project_query = bool_query( must_queries )
+        return self.es.search(index=self.PROJECT_INDEX, doc_type=self.PROJECT_TYPE, body=project_query)['hits']
+
+    def get_project(self, id):
+        project = self.es.get(index=self.PROJECT_INDEX, doc_type=self.PROJECT_TYPE, id=id)
+        return project
+
+    def delete_project(self, id):
+        self.es.delete(index=self.PROJECT_INDEX, doc_type=self.PROJECT_TYPE, id=id)
+
+    #-------
 
     def delete_index(self):
         self.es.indices.delete(self.RESUME_INDEX, ignore=[400, 404])
