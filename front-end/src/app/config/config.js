bf04d19e7566d025f58a8a5a2a27e3625d59d0cb
Macedo <macedo@avalonconsult.com>
2016-03-14 16:36:43 -0700

    return to one js file

--- front-end/src/app/config/config.js
+++ front-end/src/app/config/config.js
@@ -44,11 +44,6 @@ angular.module('AppConfig', [])
                       "employerOrgName": "Avalon Consulting, LLC",
                       "positions": [
                           {
-                              "description": "<p>This is a description of consultant's job in general at direct employer (no clientProjectId in this position).</p>",
-                              "positionType": "fulltime",
-                              "startDate": "2012-11-01"
-                          },
-                          {
                               "clientName": "Johnson, Smith, and Jones Law Firm",
                               "clientProjectId": "221a43b2c2",
                               "description": "<p>This is a description of consultant's work on this client project.</p>",
@@ -57,19 +52,8 @@ angular.module('AppConfig', [])
                               "startDate": "2013-01-01"
                           }
                       ]
-                  },
-                  {
-                      "employerOrgName": "ACME Mining",
-                      "positions": [
-                          {
-                              "description": "<p>This is a description of a former job.</p>",
-                              "endDate": "2012-11-01",
-                              "positionType": "fulltime",
-                              "startDate": "2004-01-01",
-                              "title": "Subterranian collapsation risk analyst"
-                          }
-                      ]
                   }
+
               ],
               "firstName": "ConsultantFirstName",
               "lastName": "ConsultantLastname",
