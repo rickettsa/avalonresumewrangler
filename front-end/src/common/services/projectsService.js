85df766a306bff1a3bf093088c4d211a681ad582
Macedo <macedo@avalonconsult.com>
2016-03-15 11:05:50 -0700

    minor refactor to project service, iiffe pattern and function declarations to make it more readable

--- front-end/src/common/services/projectsService.js
+++ front-end/src/common/services/projectsService.js
@@ -1,52 +1,67 @@
-"use strict";
 
 /**
  * @ngdoc service
  * @name resumeWrangler.resumeCRUDService
  * @description Create,Read,Update,Delete service for resume JSON from backend.
  */
-angular.module('resumeWrangler')
-  .service('projectsService', function($rootScope, $http, configuration) {
-    var service = {};
 
-    /**
-     * Get summary list of a specific projects
-     * @returns {promise}
-     */
-    service.fetchProject = function(projectId) {
-      return $http({
-        method: "GET",
-        url: configuration.api + '/api/projects/' + projectId + '?expand_user_info=true'
-      });
-    };
+ (function(){
+  "use strict";
 
-    /**
-     * Get summary list of all projects
-     * @returns {promise}
-     */
-    service.fetchProjects = function(skill) {
-      var url = configuration.api + '/api/projects/search?expand_user_info=true';
+  angular
+    .module('resumeWrangler')
+    .service('projectsService', projectsService)
 
-      if (!_.isEmpty(skill)){
-          url += '?project_skills=' + skill;
-      }
+    projectsService.$inject = ['$rootScope', '$http', 'configuration']
 
-      return $http({
-        method: "GET",
-        url: url
-      });
-    };
+    function ProjectService($rootScope, $http, configuration) {
 
-    service.updateProject = function(id, payload) {
-      return $http({
-        method: "PUT",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        url: configuration.api + '/api/projects/' + id,
-        data: payload
-      });
-    };
+      var service = {
+        fetchProject  : fetchProject,
+        fetchProjects : fetchProjects,
+        updateProject : updateProject
+      };
 
-    return service;
-  });
+      return service;
+
+      /**
+       * Get summary list of a specific projects
+       * @returns {promise}
+       */
+      function fetchProject(projectId) {
+        return $http({
+          method : "GET",
+          url    : configuration.api + '/api/projects/' + projectId + '?expand_user_info=true'
+        });
+      };
+
+      /**
+       * Get summary list of all projects
+       * @returns {promise}
+       */
+       function fetchProjects (skill) {
+        var url = configuration.api + '/api/projects/search?expand_user_info=true';
+
+        if (!_.isEmpty(skill)){
+            url += '?project_skills=' + skill;
+        }
+
+        return $http({
+          method : "GET",
+          url    : url
+        });
+      };
+
+       function updateProject(id, payload) {
+        return $http({
+          method  : "PUT",
+          headers : {'Content-Type': 'application/json'},
+          url     : configuration.api + '/api/projects/' + id,
+          data    : payload
+        });
+      };
+
+  }
+
+ })();
+
